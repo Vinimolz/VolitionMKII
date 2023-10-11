3fdc510cbe94915da5e2a5bf457fcde413333721
Vinimolz <76082483+Vinimolz@users.noreply.github.com>
2023-10-10 22:09:53 -0700

    Finish implementing admin product functionality

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -7,5 +7,9 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [     
     path('', views.get_products, name='get_products'),
+    path('create/', views.create_product, name='create_product'),
     path('<str:pk>/', views.get_the_product, name='get_the_product'),
+    path('delete/<str:pk>/', views.delete_product, name='delete_product'),
+    path('update/<str:pk>/', views.update_product, name='update_product'),
+    
 ]
--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -15,6 +15,7 @@ def get_products(request):
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
 def get_the_product(request, pk):
 
@@ -28,3 +29,65 @@ def get_the_product(request, pk):
         message = {'detail' : 'Product does not exists'}
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def delete_product(request, pk):
+    try:
+        product = Product.objects.get(_id=pk)        
+        product.delete()  
+        return Response('Product Deleted')
+    
+    except:
+        message = {'detail' : 'Product does not exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def create_product(request):
+
+    user = request.user
+
+    try:
+        product = Product.objects.create(
+            user = user,
+            name = 'Sample name',
+            price = 0,
+            brand = 'Sample brand',
+            countInStock = 0,
+            category = 'Sample Categoty',
+            description = ''
+        )
+        
+        serializer = ProductSerializer(product, many=False)        
+        return Response(serializer.data)
+    
+    except:
+        message = {'detail' : 'Could not create new product'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def update_product(request, pk):
+
+    data = request.data
+
+    try:
+        product = Product.objects.get(_id=pk)
+
+        product.name = data['name']
+        product.price = data['price']
+        product.brand = data['brand']
+        product.countInStock = data['countInStock']
+        product.category = data['category']
+        product.description = data['description']
+
+        product.save()
+        
+        serializer = ProductSerializer(product, many=False)
+        
+        return Response(serializer.data)
+    except:
+        message = {'detail' : 'Product could not be updated'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
